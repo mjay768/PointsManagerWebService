3a33753f424de94a2e21f6fb953be184fdc5ae9f
mjay768 <MJL!ves4Ever>
2021-02-08 00:21:05 -0600

    Features successfully added and working.

--- Readme.md
+++ Readme.md
@@ -1,9 +1,51 @@
+#Getting Started:
+
+
+
+
+Please clone the repository and make sure you have the following libraries installed. If not, please read include guide on how to install Python libraries
+1. Flask v1.1.2
+2. Flask-RESTful 0.3.8
+3. Python requests library
+4. Python re (regular expressions library)
+
+####How to install Python Libraries:
+`pip install requests` to install **requests** library<br>
+`pip install Flask` to install **Flask**. In case you have a problem installing Flask, 
+Please visit https://flask.palletsprojects.com/en/1.1.x/installation/#install-flask <br>
+`pip install flask-restful` to install **Flask-RESTful**. Visit https://flask-restful.readthedocs.io/en/latest/installation.html for help.
+
+##### GET
+
+`/`
+
+**Response**: Returns user points and data is unsorted
+
+`/points` <br>
+**Response**: Returns user points and data is sorted
+
+`/deductions` <br>
+**Response**: Returns points deducted while transactions are being added
+
+`/transactions`<br>
+
+**Response** : Returns all the transactions so far.
+
+`/deduct/<points>` <br>
+
+Parameters: points to be deducted
+
+**Response**: Returns number of points deducted from each Payer
+
+####POST
+`/<payer>/<points>/<date>`
+
+###### Parameters
+Payer name, Points, and date in the format
+`YY%MM%DD%%HH%MM%[AM/PM]`
+
 ### **What's working:**
 1. Points balance is being updated properly
 2. All transactions are being recorded
 3. Deductions are being recorded.
-4. Date can be sent as a string in the format YY%MM%DD%%HH%MM%[AM/PM]
-
-**Todo:**
-1. ~~deduct [amount] functionality yet to be implemented.~~
-2. Returning values in the required format
+4. Date can be sent as a string in the format 
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse, abort
+from flask_restful import Api, Resource, abort
 import datetime
 import re
 
+# Defining a Flask app
 app = Flask(__name__)
 api = Api(app)
+
+
 
 # Stores user points data
 user_data = {}
@@ -12,113 +15,49 @@ user_data = {}
 # Used to track transactions as they are happening
 tracker = {}
 
-
-# Deducts points specified
-@app.route('/deduct/<pts_deduct>')
-def deduct(pts_deduct):
-    # Type casting points from str to int
-    pts_deduct = int(pts_deduct)
-    temp_user_points = getBalance()
-    # Return in case given points to deduct is negative, and returning user points data
-    if int(pts_deduct) <= 0:
-        #abort(202, message="Please check points to deduct")
-        return user_data
-    # Dictionary to keep track of the deductions happening
-    deduct_track = {}
-
-    # Initializing the dictionary values to 0
-    for payer in user_data:
-        deduct_track[payer] = 0
-
-    # Getting transactions from the Class object
-    transactions = getTransactions()
-    for t in range(len(transactions)-1):
-        if pts_deduct <=0:
-            updateBalance(deduct_track)
-
-        else:
-
-            # tr has current transaction
-            tr = transactions[t]
-
-            # Unpacking the [payee, points] values
-            tr_payee, tr_points = tr[0], tr[1]
-
-            # Proceed if points to deduct is greater than the transaction points
-            if pts_deduct >=tr_points:
-                if tr_points >= 0 and tr_points <= temp_user_points[tr_payee]:
-                    pts_deduct -= tr_points
-                    deduct_track[tr_payee] -= tr_points
-                    temp_user_points[tr_payee] -= tr_points
-
-                elif tr_points < 0 and temp_user_points[tr_payee] <= 0:
-                    continue
-                elif tr_points < 0 and temp_user_points[tr_payee] > 0:
-                    diff = deduct_track[tr_payee] - tr_points
-                    deduct_track[tr_payee] = diff
-                    pts_deduct += abs(tr_points)
-                    temp_user_points[tr_payee] += abs(tr_points)
-                elif tr_points > 0 and tr_points > temp_user_points[tr_payee] and temp_user_points[tr_payee] != 0:
-                    pts_deduct -= temp_user_points[tr_payee]
-                    deduct_track[tr_payee] -= temp_user_points[tr_payee]
-                    temp_user_points[tr_payee] = 0
-
-                elif tr_points > 0 and temp_user_points[tr_payee] == 0:
-                    continue
-
-            elif pts_deduct < tr_points:
-                if pts_deduct < temp_user_points[tr_payee]:
-                    deduct_track[tr_payee] -= pts_deduct
-                    temp_user_points[tr_payee] -= pts_deduct
-                    pts_deduct = 0
-                elif pts_deduct > temp_user_points[tr_payee]:
-                    pts_deduct -= temp_user_points[tr_payee]
-                    temp_user_points[tr_payee] = 0
-
-    #abort(200, message="Deduction complete")
-    print("Pts to deduct: ",pts_deduct)
-    print("User data: ",user_data)
-    if pts_deduct > 0:
-        updateBalance(temp_user_points)
-    return deduct_track
-
-
+# Dictionary to keep track of the deductions happening during POST transactions
+deduct_track = {}
 
 # Retrieves the list of all transactions
-@app.route('/gettransactions')
+@app.route('/transactions')
 def getTransactions():
-    tr = Transaction()
+    tr = PointsManager()
     tr_dict = tr.setTransactions()
+    del tr
     return tr_dict
+
 
 # Updates balance after points are deducted
 def updateBalance(new_points_balance):
-    #print(deduct_track)
+
     for payer in (user_data):
-        #if user_data[payer] >0:
         user_data[payer] = new_points_balance[payer]
     print("UD from updateBal: ",user_data)
     return True
 
 # Retrieves user points balance
-@app.route('/getpointsbalance')
+@app.route('/points')
 def getBalance():
     return user_data
 
-class Transaction(Resource):
-    transactions = []
+class PointsManager(Resource):
 
+    transactions = []
     # Retrieves user points balance
-    @app.route("/")
-    def get():
+    def get(self):
         return user_data
 
     def post(self,payer,points,date):
+        self.addPoints(payer, points, date)
+
+        return self.transactions
+
+    def addPoints(self, payer, points, date):
         date = self.parseDate(date)
         points = int(points)
         if payer not in user_data:
             if points < 0:
-                abort (404, message='User does not exist, unable to deduct')
+                abort(404, message='User does not exist')
             else:
                 user_data[payer] = points
                 self.transactions += [[payer, int(points), date]]
@@ -129,8 +68,77 @@ class Transaction(Resource):
                 self.transactions += [[payer, int(points), date]]
                 if points < 0:
                     tracker[payer] += points
-            else: abort(404, message= "Insufficient points to deduct")
-        return self.transactions
+            else:
+                abort(404, message="Insufficient points to deduct")
+    # Deducts points specified
+    @app.route('/deduct/<pts_deduct>')
+    def deduct(pts_deduct):
+        # Type casting points from str to int
+        pts_deduct = int(pts_deduct)
+        temp_user_points = getBalance()
+        # Return in case given points to deduct is negative, and returning user points data
+        if int(pts_deduct) <= 0:
+            return {404,"Error: Please check the amount"}
+
+        # Initializing the dictionary values to 0
+        for payer in user_data:
+            deduct_track[payer] = 0
+
+        # Getting transactions from the Class object
+        transactions = getTransactions()
+        for t in range(len(transactions) - 1):
+            if pts_deduct <= 0:
+                updateBalance(deduct_track)
+
+            else:
+
+                # tr has current transaction
+                tr = transactions[t]
+
+                # Unpacking the [payee, points] values
+                tr_payee, tr_points = tr[0], tr[1]
+
+                # Proceed if points to deduct is greater than the transaction points
+                if pts_deduct >= tr_points:
+                    if tr_points >= 0 and tr_points <= temp_user_points[tr_payee]:
+                        pts_deduct -= tr_points
+                        deduct_track[tr_payee] -= tr_points
+                        temp_user_points[tr_payee] -= tr_points
+
+
+                    elif tr_points < 0 and temp_user_points[tr_payee] <= 0:
+                        continue
+
+
+                    elif tr_points < 0 and temp_user_points[tr_payee] > 0:
+                        diff = deduct_track[tr_payee] - tr_points
+                        deduct_track[tr_payee] = diff
+                        pts_deduct += abs(tr_points)
+                        temp_user_points[tr_payee] += abs(tr_points)
+
+
+                    elif tr_points > 0 and tr_points > temp_user_points[tr_payee] and temp_user_points[tr_payee] != 0:
+                        pts_deduct -= temp_user_points[tr_payee]
+                        deduct_track[tr_payee] -= temp_user_points[tr_payee]
+                        temp_user_points[tr_payee] = 0
+
+                    elif tr_points > 0 and temp_user_points[tr_payee] == 0:
+                        continue
+
+                elif pts_deduct < tr_points:
+                    if pts_deduct < temp_user_points[tr_payee]:
+                        deduct_track[tr_payee] -= pts_deduct
+                        temp_user_points[tr_payee] -= pts_deduct
+                        pts_deduct = 0
+                    elif pts_deduct > temp_user_points[tr_payee]:
+                        pts_deduct -= temp_user_points[tr_payee]
+                        temp_user_points[tr_payee] = 0
+
+        print("Points to deduct: ", pts_deduct)
+        print("User data: ", user_data)
+        if pts_deduct > 0:
+            updateBalance(temp_user_points)
+        return deduct_track
 
     # Transaction setter
     def setTransactions(self):
@@ -141,7 +149,7 @@ class Transaction(Resource):
 
     # Retrieves the deductions happened from transactions
     @app.route('/deductions')
-    def getDeductions():
+    def deductions():
         return tracker
 
     # Date parser from the arguments
@@ -152,7 +160,7 @@ class Transaction(Resource):
         string = string[3:5] + '/' + string[6:8] + " " + string[10:12] + ":" + string[13:]
         return string
 
-api.add_resource(Transaction,"/<string:payer>/<string:points>/<string:date>")
+api.add_resource(PointsManager,"/<string:payer>/<string:points>/<string:date>","/","/deductions")
 
 if __name__ == '__main__':
     app.run(debug = True)
